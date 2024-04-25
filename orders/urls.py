151c6d21059fdf39eca80fffb0cf88f6c34e7f05
hyunsu <hyunssu1130@naver.com>
2024-04-25 16:18:56 +0900

    sales_transaction

--- orders/urls.py
+++ orders/urls.py
@@ -9,5 +9,10 @@ app_name = "orders"
 #입찰 참여 - > 입찰가 입력 (-> 마일리지 차감) -> 입찰 참여중 목록에 올라감(product table 참조)
 #입찰 완료 -> Bid 테이블에 새로운 행 생성 -> 입찰 완료 상품 목록에 추가 
 urlpatterns = [
-    # path("", views.MenuLV.as_view(), name="index"),
+    path('purchase_history/', views.purchase_history, name='purchase_history'),
+    path("purchase_history/ing/", views.purchase_history_ing, name="purchase_history_ing"),
+    path("purchase_history/end/", views.purchase_history_end, name="purchase_history_end"),
+    path('sales_history/', views.sales_history, name='sales_history'),
+    path('sales_history/ing/', views.sales_history_ing, name='sales_history_ing'),
+    path('sales_history/end/', views.sales_history_end, name='sales_history_end'),
 ]
