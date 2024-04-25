015c0eefa1ab2865dd9e5287ca2c1b9e4377bd9c
mins1120 <162934135+mins1120@users.noreply.github.com>
2024-04-25 10:36:42 +0900

    기능 추가(admin 수정)
    기능 추가(admin 수정)

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('registrater/', views.ProductCreateView.as_view(), name='product_registrater'),
 ]
