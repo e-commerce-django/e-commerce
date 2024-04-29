c8030b9be65db1ab955417afe677fc5daddeac99
hellojunho <jo62698476@gmail.com>
2024-04-29 09:54:11 +0900

    feat: [#29] login, registration 구현

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(max_length=200, unique=True)
+    phone_number = models.CharField(max_length=20)
+    user_type = models.BooleanField(default=True)
+    address = models.CharField(max_length=255)
+    username = models.CharField(max_length=30, unique=False)
+    
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'user'
--- accounts/views.py
+++ accounts/views.py
@@ -1,54 +1,77 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from products.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.views.generic import View
 from django.http import HttpResponse
 from django.contrib import messages
+from .forms import UserForm, UserLoginForm
+from django.contrib.auth.hashers import make_password, check_password
+
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'accounts/registration.html')
-
+        if request.user.is_authenticated:  # 이미 로그인된 경우
+            return redirect('/')  # 루트 URL로 리다이렉션
+        form = UserForm()
+        return render(request, 'accounts/registration.html', {'form': form})
 
     def post(self, request):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        address = request.POST.get('address')
+        if request.user.is_authenticated:  # 이미 로그인된 경우
+            return redirect('/')  # 루트 URL로 리다이렉션
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # 비밀번호를 해시하여 저장
+            password = form.cleaned_data['password']
+            hashed_password = make_password(password)
+            print("password: ", password)
+            print("hashed_password: ", hashed_password)
+            
+            # 새로운 사용자 생성
+            user = User(
+                email=form.cleaned_data['email'],
+                password=hashed_password,
+                username=form.cleaned_data['username'],
+                address=form.cleaned_data['address'],
+                phone_number=form.cleaned_data['phone_number'],
+            )
+            user.save()
 
-        if password != confirm_password:
-            messages.error(request, '비밀번호가 일치하지 않습니다.')
-            return redirect('/')
-
-        # 회원 생성
-        user = User.objects.create_user(username=email, email=email, password=password)
-        user.first_name = name
-        user.save()
-
-        # 추가 필드 저장
-        user_profile = user.profile
-        user_profile.phone = phone
-        user_profile.address = address
-        user_profile.save()
-
-        messages.success(request, '회원가입이 완료되었습니다. 로그인해주세요.')
-        return redirect('login')
+            # 회원가입 성공 후, 로그인 페이지로 이동하거나 다른 페이지로 리다이렉트
+            return redirect('/')  # 로그인 페이지로 이동하도록 설정 (login은 로그인 URL의 이름입니다)
+        else:
+            print("폼 유효성 검사 실패:", form.errors)
+            return render(request, 'accounts/registration.html', {'form': form})
 
 class LoginView(View):
     def get(self, request):
-        return render(request, 'accounts/login.html')
+        if request.user.is_authenticated:
+            return redirect('/')  # 이미 로그인된 경우 루트 페이지로 리다이렉션
+        form = UserLoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
 
     def post(self, request):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user = authenticate(username=email, password=password)
-
-        if user is not None:
-            login(request, user)
-            return HttpResponse('로그인 성공')
-        else:
-            messages.error(request, '이메일 또는 비밀번호가 잘못되었습니다.')
-            return redirect('/')
+        if request.user.is_authenticated:  # 이미 로그인된 경우
+            return redirect('/')  # 루트 URL로 리다이렉션
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            try:
+                user = User.objects.get(email=email)
+                # 인증
+                user = authenticate(request, email=email, password=password)
+                if user is not None:
+                    # 인증에 성공하면 로그인
+                    login(request, user)
+                    print("로그인 성공")
+                    return redirect('/')  # 로그인 후 리다이렉트할 URL
+                else:
+                    # 인증에 실패하면 에러메시지 표시
+                    print("비밀번호가 일치하지 않습니다.")
+                    form.add_error('password', '비밀번호가 일치하지 않습니다.')
+            except User.DoesNotExist as e:
+                # 해당 이메일로 등록된 사용자가 없는 경우
+                print(e)
+                form.add_error('email', '등록되지 않은 이메일입니다.')
+        return render(request, 'accounts/login.html', {'form': form})
