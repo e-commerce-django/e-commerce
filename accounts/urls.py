33110d1b499954e0317071520477f948116a1735
hellojunho <jo62698476@gmail.com>
2024-04-30 17:48:22 +0900

    feat: [#42] 마이페이지, 로그아웃 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, MyPageView, LogoutView
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("mypage/", MyPageView.as_view(), name="mypage"),
 ]
