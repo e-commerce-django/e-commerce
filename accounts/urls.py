04146166baba86a92220399e80949fee7de469bb
hellojunho <jo62698476@gmail.com>
2024-05-07 14:43:50 +0900

    feat: [#57] email authenticate while join

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, MyPageView, LogoutView, ModifyView, withdraw
+from .views import RegisterView, LoginView, MyPageView, LogoutView, ModifyView, withdraw, send_email
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('register/verification/', send_email, name='send_email'),
     path('login/', LoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("mypage/", MyPageView.as_view(), name="mypage"),
