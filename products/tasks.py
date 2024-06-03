3a34bee39e726c11e31881fa4431aabb4eacf93f
unknown <mskang1120@naver.com>
2024-06-03 09:29:41 +0900

    email feature

--- products/tasks.py
+++ products/tasks.py
@@ -26,17 +26,17 @@ def update_product_status():
         current_time = timezone.now()
         logger.info("Updating product status...")
 
-        # # 시간 체크, 이메일 전송 처리
-        # intervals = [24, 12, 6, 1]
-        # for hours in intervals:
-        #     time_threshold = current_time + timedelta(hours=hours)
-        #     ongoing_email_products = Product.objects.filter(
-        #         auction_end_time__lte=time_threshold,
-        #         auction_end_time__gt=current_time,
-        #         product_status=True
-        #     )            
-        #     for product in ongoing_email_products:
-        #         send_auction_alert_emails(product, hours)
+        # 시간 체크, 이메일 전송 처리
+        intervals = [24, 12, 6, 1]
+        for hours in intervals:
+            time_threshold = current_time + timedelta(hours=hours)
+            ongoing_email_products = Product.objects.filter(
+                auction_end_time__lte=time_threshold + timedelta(seconds=5),
+                auction_end_time__gte=time_threshold - timedelta(seconds=5),
+                product_status=True
+            )
+            for product in ongoing_email_products:
+                send_auction_alert_emails(product, hours)
 
         # 경매 종료된 상품 처리
         expired_products = Product.objects.filter(
@@ -96,16 +96,33 @@ def update_product_status():
 
 def send_auction_alert_emails(product, hours):
     bidders = Bidder.objects.filter(product_id=product)
-    recipient_emails = [bidder.bidder_id.email for bidder in bidders if bidder.bidder_id is not None]
 
-    send_email(recipient_emails, product, hours)
+    # 최고 입찰자가 있는 경우
+    winner = None
+    if product.present_max_bidder_id is not None:
+        winner = User.objects.get(id=product.present_max_bidder_id)
+
+    recipient_emails = [bidder.bidder_id.email for bidder in bidders if bidder.bidder_id is not None and  bidder.bidder_id.id != product.present_max_bidder_id]
+
+    #모든 입찰자에게 이메일 전송
+    if recipient_emails:
+        send_email(recipient_emails, product, hours)
+
+    # 최고 입찰자에게 이메일 전송
+    if winner:
+        send_winner_email(winner.email, product, hours)
     
     
 
 def send_email(recipient_email, product, hours):
-    subject = f"{product.name} 상품의 입찰이 {hours}시간 남았습니다."
-    message = f"{product.name} 상품의 입찰이 {hours}시간 남았습니다. 구매할 기회를 놓치지 마세요!"
-    from_email = os.getenv("EMAIL_HOST_ID")
+    if hours == 0:
+        subject = f"{product.name} 상품의 입찰이 종료되었습니다."
+        message = f"{product.name} 상품의 입찰이 종료되었습니다. 입찰에 실패하였습니다! 다음 기회를 노리세요!"
+        from_email = os.getenv("EMAIL_HOST_ID")
+    else:
+        subject = f"{product.name} 상품의 입찰이 {hours}시간 남았습니다."
+        message = f"{product.name} 상품의 입찰이 {hours}시간 남았습니다. 구매할 기회를 놓치지 마세요!"
+        from_email = os.getenv("EMAIL_HOST_ID")
     msg = EmailMultiAlternatives(
           subject,
           message,
@@ -117,4 +134,28 @@ def send_email(recipient_email, product, hours):
         logger.info(f"Email sent to {recipient_email} for product {product.id}")
     except Exception as e:
         logger.error(f"Failed to send email to {recipient_email} for product {product.id}: {e}")
+        logger.error(traceback.format_exc())
+
+def send_winner_email(winner_email, product, hours):
+    if hours == 0:
+        subject = f"{product.name} 상품의 입찰이 종료되었습니다."
+        message = f"{product.name} 상품의 입찰이 종료되었습니다. 축하합니다! 입찰에 성공하였습니다!"
+        from_email = os.getenv("EMAIL_HOST_ID")
+    else:
+        subject = f"{product.name} 상품의 입찰이 {hours}시간 남았습니다."
+        message = f"{product.name} 상품의 입찰이 {hours}시간 남았습니다. 현재 최고 입찰자입니다!"
+        from_email = os.getenv("EMAIL_HOST_ID")
+    
+    from_email = os.getenv("EMAIL_HOST_ID")
+    msg = EmailMultiAlternatives(
+        subject,
+        message,
+        from_email
+    )
+    msg.to = [winner_email]
+    try:
+        msg.send()
+        logger.info(f"Email sent to {winner_email} for product {product.id}")
+    except Exception as e:
+        logger.error(f"Failed to send email to {winner_email} for product {product.id}: {e}")
         logger.error(traceback.format_exc())
